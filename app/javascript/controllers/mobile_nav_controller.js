b906cdd65ff7d6a779b36a57e2211053968024ab
Dooklin <elias.schinkinger06@gmail.com>
2025-09-04 19:58:49 +0200

    dark when header open

--- app/javascript/controllers/mobile_nav_controller.js
+++ app/javascript/controllers/mobile_nav_controller.js
@@ -1,10 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["links", "burgerLine"]
+  static targets = ["links", "burgerLine", "plaque"]
 
   toggle() {
     this.linksTarget.classList.toggle("mobile-hidden")
+    this.plaqueTarget.classList.toggle("darkened")
 
     if (this.burgerLineTargets.length >= 3) {
       this.burgerLineTargets[0].classList.toggle("short")
