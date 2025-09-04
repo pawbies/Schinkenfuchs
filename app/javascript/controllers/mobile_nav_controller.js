80e77006196e783b0d7523fa4a71ec2dfdd9defa
Dooklin <elias.schinkinger06@gmail.com>
2025-09-04 19:51:11 +0200

    Mobile nav

---
+++ app/javascript/controllers/mobile_nav_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["links", "burgerLine"]
+
+  toggle() {
+    this.linksTarget.classList.toggle("mobile-hidden")
+
+    if (this.burgerLineTargets.length >= 3) {
+      this.burgerLineTargets[0].classList.toggle("short")
+      this.burgerLineTargets[2].classList.toggle("short")
+    }
+  }
+}
