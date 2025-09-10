bd5325814459d13fae1a84443237bf6b26063e4c
pawbies <alex.fuchs@pawbies.net>
2025-09-10 18:58:23 +0200

    save theme

--- app/javascript/controllers/bulb_switch_controller.js
+++ app/javascript/controllers/bulb_switch_controller.js
@@ -4,16 +4,19 @@ export default class extends Controller {
   static targets = ["light", "dark"]
 
   connect() {
-    document.documentElement.setAttribute("data-theme", "dark");
+    if ((localStorage.getItem("theme") || "dark") == "light") this.light();
+    else this.dark();
   }
 
   dark() {
+    localStorage.setItem("theme", "dark");
     document.documentElement.setAttribute("data-theme", "dark");
     this.lightTarget.classList.add("mode-switch-hidden");
     this.darkTarget.classList.remove("mode-switch-hidden");
   }
 
   light() {
+    localStorage.setItem("theme", "light");
     document.documentElement.setAttribute("data-theme", "light");
     this.lightTarget.classList.remove("mode-switch-hidden");
     this.darkTarget.classList.add("mode-switch-hidden");
