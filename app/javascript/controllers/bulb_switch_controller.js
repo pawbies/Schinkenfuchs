55992b62cac352811a4418630e2309eb94a71eab
Dooklin <elias.schinkinger06@gmail.com>
2025-09-10 16:02:30 +0200

    light-darkmode try

--- app/javascript/controllers/bulb_switch_controller.js
+++ app/javascript/controllers/bulb_switch_controller.js
@@ -1,14 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "light", "dark" ];
+  static targets = ["light", "dark"]
+
+  connect() {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
 
   dark() {
+    document.documentElement.setAttribute("data-theme", "dark");
     this.lightTarget.classList.add("mode-switch-hidden");
     this.darkTarget.classList.remove("mode-switch-hidden");
   }
 
   light() {
+    document.documentElement.setAttribute("data-theme", "light");
     this.lightTarget.classList.remove("mode-switch-hidden");
     this.darkTarget.classList.add("mode-switch-hidden");
   }
