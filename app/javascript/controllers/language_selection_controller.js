14e6b2c2915f281a181a62d4152b06b205ed78b8
Dooklin <elias.schinkinger06@gmail.com>
2025-09-26 13:17:27 +0200

    The Whambulance Is Here To Pick You Up

--- app/javascript/controllers/language_selection_controller.js
+++ app/javascript/controllers/language_selection_controller.js
@@ -3,12 +3,30 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["options"]
 
+  /* Close when clicking anywhere else */
+  /* (Ignore this, Mr. 19 years old)*/
   connect() {
     document.addEventListener("click", this.closeOnClickAway)
   }
 
   disconnect() {
     document.removeEventListener("click", this.closeOnClickAway)
+  }
+
+  closeOnClickAway = (event) => {
+    if (!this.element.contains(event.target)) {
+      this.optionsTarget.classList.add("hidden");
+    }
+  }
+
+  /* needed fo Pagespeed insight, trust */
+  update(event) {
+    const url = event.target.value
+    if (window.Turbo) {
+      Turbo.visit(url)
+    } else {
+      window.location = url
+    }
   }
 
   toggle(event) {
@@ -20,11 +38,5 @@ export default class extends Controller {
     const url = event.target.dataset.value;
     this.optionsTarget.classList.add("hidden");
     if (window.Turbo) { Turbo.visit(url) } else { window.location = url }
-  }
-
-  closeOnClickAway = (event) => {
-    if (!this.element.contains(event.target)) {
-      this.optionsTarget.classList.add("hidden");
-    }
   }
 }
