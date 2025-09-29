bce484e90fe47e0667866fe99091ec9c4db6b8f0
pawbies <alex.fuchs@pawbies.net>
2025-09-29 14:48:53 +0200

    semicolons

--- app/javascript/controllers/reveal_controller.js
+++ app/javascript/controllers/reveal_controller.js
@@ -1,19 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = { class: String }
+  static values = { class: String };
 
   connect() {
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          const visibleClass = this.classValue || "is-visible"
-          entry.target.classList.add(...visibleClass.split(" "))
-          observer.unobserve(entry.target)
+          const visibleClass = this.classValue || "is-visible";
+          entry.target.classList.add(...visibleClass.split(" "));
+          observer.unobserve(entry.target);
         }
       })
-    }, { threshold: 0.2 })
+    }, { threshold: 0.2 });
 
-    observer.observe(this.element)
+    observer.observe(this.element);
   }
 }
