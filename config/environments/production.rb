0e775ec96508a98a1d190890e403f5bb94a92d5b
pawbies <alex.fuchs@pawbies.net>
2025-10-01 11:46:11 +0200

    disable fly domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,11 +80,12 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    "schinkenfuchs.com",     # Allow requests from example.com
+    "www.schinkenfuchs.com"
+    # /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+  ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } } # could cause issues on fly.io not sure tho
 end
