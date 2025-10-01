1d4d68265bad3bec64d0f4c4bf761058440d2030
Dooklin <elias.schinkinger06@gmail.com>
2025-10-01 15:51:28 +0200

    Login js

---
+++ app/javascript/controllers/fade_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["elem"];
+
+  connect() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const from = urlParams.get("from");
+
+    if (from === "forgot") {
+      const first = this.elemTargets[0];
+      const last = this.elemTargets[this.elemTargets.length - 1];
+
+      if (first) {
+        first.classList.add("show");
+      }
+      this.animate([last]);
+    } else {
+      this.animate(this.elemTargets);
+    }
+  }
+
+  animate(elems) {
+    requestAnimationFrame(() => {
+      elems.forEach((el, i) => {
+        setTimeout(() => {
+          el.classList.add("show");
+        }, i * 150);
+      });
+    });
+  }
+}
