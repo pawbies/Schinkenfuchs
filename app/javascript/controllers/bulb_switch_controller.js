f76d2c76d75677ed7e82a02abd11e3d18edde505
Dooklin <elias.schinkinger06@gmail.com>
2025-10-09 16:23:01 +0200

    :/ github change icon

--- app/javascript/controllers/bulb_switch_controller.js
+++ app/javascript/controllers/bulb_switch_controller.js
@@ -4,21 +4,36 @@ export default class extends Controller {
   static targets = ["light", "dark"];
 
   connect() {
-    if ((localStorage.getItem("theme") || "dark") == "light") this.light();
-    else this.dark();
+    this.updateIcons();
+    window.addEventListener("themechange", this.updateIcons);
+  }
+
+  disconnect() {
+    window.removeEventListener("themechange", this.updateIcons);
+  }
+
+  updateIcons = () => {
+    const theme = (localStorage.getItem("theme") || "dark");
+    if (theme === "light") {
+      this.lightTargets.forEach(el => el.classList.remove("mode-switch-hidden"));
+      this.darkTargets.forEach(el => el.classList.add("mode-switch-hidden"));
+    } else {
+      this.lightTargets.forEach(el => el.classList.add("mode-switch-hidden"));
+      this.darkTargets.forEach(el => el.classList.remove("mode-switch-hidden"));
+    }
   }
 
   dark() {
     localStorage.setItem("theme", "dark");
     document.documentElement.setAttribute("data-theme", "dark");
-    this.lightTarget.classList.add("mode-switch-hidden");
-    this.darkTarget.classList.remove("mode-switch-hidden");
+    this.updateIcons();
+    window.dispatchEvent(new Event("themechange"));
   }
 
   light() {
     localStorage.setItem("theme", "light");
     document.documentElement.setAttribute("data-theme", "light");
-    this.lightTarget.classList.remove("mode-switch-hidden");
-    this.darkTarget.classList.add("mode-switch-hidden");
+    this.updateIcons();
+    window.dispatchEvent(new Event("themechange"));
   }
 }
