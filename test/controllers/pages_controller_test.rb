f8312c1860c36a3712dd947da5bdd7c753f6fa2a
pawbies <alex.fuchs@pawbies.net>
2025-10-20 15:04:06 +0200

    test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,4 +5,14 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+
+  test "should get process" do
+    get process_url
+    assert_response :success
+  end
+
+  test "should get timeline" do
+    get timeline_url
+    assert_response :success
+  end
 end
