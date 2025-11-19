171097b24e8a71abf87be6734c493dbf78c1f082
pawbies <alex.fuchs@pawbies.net>
2025-11-19 20:25:41 +0100

    somewhat processing request

--- app/controllers/website/requests_controller.rb
+++ app/controllers/website/requests_controller.rb
@@ -6,6 +6,18 @@ class Website::RequestsController < ApplicationController
   end
 
   def create
-    redirect_to root_path
+    @website_request = Website::Request.new website_request_params
+
+    if @website_request.save
+      redirect_to root_path, notice: "Thank you for your request, you will receive an email to verify it shortly"
+      WebsiteRequestsMailer.verify(@website_request).deliver_later
+    else
+      render :new, status: :unprocessable_content
+    end
   end
+
+  private
+    def website_request_params
+      params.expect(website_request: [ :title, :plan, :size, :description, :email_address ])
+    end
 end
