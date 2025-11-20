b56a777651a7dfe9544cd83ccf54cb737403101d
pawbies <alex.fuchs@pawbies.net>
2025-11-20 16:18:39 +0100

    verifications

--- app/controllers/website/verifications_controller.rb
+++ app/controllers/website/verifications_controller.rb
@@ -3,10 +3,20 @@ class Website::VerificationsController < ApplicationController
   before_action :get_request_by_token, only: %i[ new create ]
 
   def new
+    @user = User.new
   end
 
   def create
-    redirect_to root_path
+    @user = User.new(**user_params, email_address: @website_request.email_address, role: "user")
+    if params[:accepted_tos] != "1"
+      flash.now[:alert] = "Please accept the TOS to continue."
+      render :new, status: :forbidden
+    elsif @user.save
+      # TODO: link request to user
+      redirect_to root_path, notice: "Your request is now being processed, check the status with your login credentials."
+    else
+      render :new, status: :unprocessable_content
+    end
   end
 
   private
@@ -16,5 +26,9 @@ class Website::VerificationsController < ApplicationController
       rescue ActiveSupport::MessageVerifier::InvalidSignature
         redirect_back fallback_location: root_path, alert: "Invalid or expired token."
       end
+    end
+
+    def user_params
+      params.expect(user: [ :username, :password, :password_confirmation ])
     end
 end
